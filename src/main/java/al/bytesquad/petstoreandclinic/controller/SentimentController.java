c7c9af73309e646aa6ef485f8efacb40056feda8
Athirah <athirahlokman23@gmail.com>
2023-12-21 17:01:34 +0800

    Draft - Intsall numpy & stanford-corenlp

--- src/main/java/al/bytesquad/petstoreandclinic/controller/SentimentController.java
+++ src/main/java/al/bytesquad/petstoreandclinic/controller/SentimentController.java
@@ -1,13 +1,38 @@
 package al.bytesquad.petstoreandclinic.controller;
 
-//services and entity for feedback and sentiment
-import al.bytesquad.petstoreandclinic.entity.FeedbackEntity;
+//service
 import al.bytesquad.petstoreandclinic.service.FeedbackService;
 // import al.bytesquad.petstoreandclinic.service.exception.SentimentService;
-import al.bytesquad.petstoreandclinic.service.exception.ResourceNotFoundException;
+import al.bytesquad.petstoreandclinic.service.UserService;
 
+//entity
+import al.bytesquad.petstoreandclinic.entity.FeedbackEntity;
+
+//other imports
+import org.springframework.beans.factory.annotation.Autowired;
+import al.bytesquad.petstoreandclinic.service.exception.ResourceNotFoundException;
 import org.springframework.web.bind.annotation.*;
 
-public class SentimentController {
-    
-}
+import java.security.Principal;
+import java.util.ArrayList;
+import java.util.List;
+
+// @RestController
+// @RequestMapping("/nlp")
+
+// public class SentimentController {
+//     private final SentimentService sentimentService;
+
+//     @Autowired
+//     public SentimentController(SentimentService sentimentService) {
+//         this.sentimentService = sentimentService;
+//     }
+
+//     @PostMapping("/sentiment")
+//     @CrossOrigin(origins = "http://localhost:3000")
+//     public String getSentiment(@RequestBody String text)
+//     {
+//         String res = sentimentService.getSentiment(text);
+//         return res;
+//     }
+// }
--- src/main/java/al/bytesquad/petstoreandclinic/service/SentimentService.java
+++ src/main/java/al/bytesquad/petstoreandclinic/service/SentimentService.java
@@ -2,10 +2,23 @@
 
 package al.bytesquad.petstoreandclinic.service;
 
-import java.util.List;
-
 import org.modelmapper.ModelMapper;
+import org.modelmapper.ModelMapper; 
+import org.modelmapper.PropertyMap;
 import org.springframework.beans.factory.annotation.Autowired;
+import java.util.HashMap;
+import java.util.Map;
+
+
+//entity
+import al.bytesquad.petstoreandclinic.entity.User;
+import al.bytesquad.petstoreandclinic.payload.entityDTO.UserDTO;
+
+//repo
+import al.bytesquad.petstoreandclinic.repository.UserRepository;
+
+
+//sus
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -17,5 +30,20 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import al.bytesquad.petstoreandclinic.entity.User;
 import al.bytesquad.petstoreandclinic.repository.UserRepository;
 import al.bytesquad.petstoreandclinic.service.exception.ResourceNotFoundException;
+
+
+@Service
+public class SentimentService {
+
+    @Autowired
+    public SentimentService() {
+    }
+
+    // public String getSentiment(String text) {
+    //     String res = sentiment(text);
+    //     return res;
+    // }
+
+}
 
 
